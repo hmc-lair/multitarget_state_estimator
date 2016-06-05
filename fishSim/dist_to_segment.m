346fbca214d95447bdb42a65ab7827d9a0f9b546
Cherie Ho <ho.cherie@gmail.com>
2016-06-04 22:18:19 -0700

    Implemented own dist_to_segment

--- fishSim/dist_to_segment.m
+++ fishSim/dist_to_segment.m
@@ -1,28 +1,29 @@
-function dist = dist_to_segment(px, py, x1, y1, x2, y2)
+function distance = dist_to_segment(px, py, line_start, line_end)
 % px and py: Point
 % x1 and y1: Segment Start, x2 and y2: Segment End
 
-v = [x2 y2] - [x1 y1];
-w = [px py] - [x1 y1];
+v = line_end - line_start;
+w = [px py] - line_start;
 
 c1 = dot(v, w);
 
 
-dist = 0;
+distance = 0;
 
 if c1 <= 0
-    dist = distance(px, py, x1, y1);
-end
+    dist_shark = dist(px, py, line_start(1), line_start(2));
 
 else
     c2 = dot(v,v);
     if c2 <= c1
-        dist = distance(px, py, x2, y2);
-    end
+        dist_shark = dist(px, py, line_end(1), line_end(2));
     
     else
         b = c1/c2;
-        pb = [x1 y1] + b * v;
-        dist = distance(px, py, Pb(1), Pb(2));
+        Pb = line_start + b * v;
+        dist_shark = dist(px, py, Pb(1), Pb(2));
+    end
 end
+
+distance = dist_shark;
 end
