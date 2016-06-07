23b870fb5c38d01843e8b37fdf04a1adde087f78
Cherie Ho <chho@hmc.edu>
2016-06-07 13:14:14 -0700

    Changed maxdist and sumdist fit

--- fishSim/fit_maxdist.m
+++ fishSim/fit_maxdist.m
@@ -1,4 +1,5 @@
 function max_dist = fit_maxdist(numshark)
 
-max_dist = 16.48 * numshark^0.03116 - 16.11;
+max_dist = 24.66 * numshark^0.03753 - 25.05;
+% max_dist = 16.48 * numshark^0.03116 - 16.11;
 end
