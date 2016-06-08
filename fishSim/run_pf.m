ce93a9fed91a200375f9359f07d2f3d17e9e98dd
Cherie Ho <ho.cherie@gmail.com>
2016-06-08 15:51:45 -0700

    Start percent tag experiment for et_num_shark

--- fishSim/run_pf.m
+++ fishSim/run_pf.m
@@ -12,32 +12,46 @@ clf
 
 % Line endpoints from actual data (from Kim)
 % N_fish = 110;
-N_tag = N_fish;
+% N_tag = N_fish;
 
-ts_pf = 800;
+tag_list =linspace(10,50,5);
+ts_pf = 100;
+
+tag_list_size = size(tag_list, 2);
+act_error_list = zeros(ts_pf, tag_list_size);
+est_error_list = zeros(ts_pf, tag_list_size);
+numshark_est_list = zeros(ts_pf, tag_list_size);
+
+
 
 %Variables
 % ts_pf = 1000;
 % N_tag = 100;
-[act_error, est_error, error, numshark_est] = att_pf(x, y, t, N_tag, LINE_START, LINE_END, ts_pf);
-
+for i = 1:tag_list_size
+    N_tag = tag_list(i)
+    [act_error, est_error, error, numshark_est] = att_pf(x, y, t, N_tag, LINE_START, LINE_END, ts_pf);
+    act_error_list(:,i) = act_error;
+    est_error_list(:,i) = est_error;
+    error_list(:,i) = error;
+    numshark_est_list(:,i) = numshark_est;
+end
 % Plot Performance of attraction line PF
 subplot(3,1,1)
 hold on
-plot(act_error, '.')
-plot(est_error, '.')
+plot(act_error_list, '.')
+plot(est_error_list, '.')
 legend('Actual Line', 'Estimated Line')
 title(sprintf('Comparison of Sum of Distance to Act and Est Line for %d Sharks with sigma=1 and uni(0,0.1)', N_fish));
 hold off
 
 subplot(3,1,2)
-plot(error, '.')
+plot(error_list, '.')
 title('Performance Error (\Sigma sqrt((dist\_act\_i - dist\_est\_i)^2/numshark)))')
 
 subplot(3,1,3)
 hold on
 plot([0 ts_pf], [N_fish N_fish]);
-plot(numshark_est, '.');
+plot(numshark_est_list, '.');
 ylim([0 200]);
 legend('Actual', 'Estimated')
 title('Comparison of Actual and Estimated Number of Sharks')
