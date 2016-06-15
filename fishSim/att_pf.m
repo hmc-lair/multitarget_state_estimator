6666d820f46da32712c8d467c14be3c9fab0194d
Cherie Ho <ho.cherie@gmail.com>
2016-06-15 10:32:09 -0700

    Added robots that listens to tags

--- fishSim/att_pf.m
+++ fishSim/att_pf.m
@@ -1,21 +1,27 @@
 
-function [act_error, est_error, error, numshark_est] = att_pf(x, y, t, N_tagged, LINE_START, LINE_END, TS_PF, show_visualization)
+function [act_error, est_error, error, numshark_est] = att_pf(x, y, t, LINE_START, LINE_END, TS_PF, show_visualization)
 
 % PF Constants
 Height = 10;
-Width = 10;
+Width = 50;
 N_part = 100;
 Sigma_mean = 0.5;
 N_fish = size(x,2);
-x_tagged = x(:, 1:N_tagged);
-y_tagged = y(:, 1:N_tagged);
-t_tagged = t(:, 1:N_tagged);
+% x_tagged = x(:, 1:N_tagged);
+% y_tagged = y(:, 1:N_tagged);
+% t_tagged = t(:, 1:N_tagged);
+
+robots = [-25 0 0;
+           25 0 0];
+N_robots = 2;
+range = 10;
 
 numshark_sd = 0.65;
 
 
 
 % Initialize States
+% robots = initRobots(
 p = initParticles(Height, Width, N_part);
 est_error = zeros(TS_PF,1);
 act_error = zeros(TS_PF,1);
@@ -27,18 +33,25 @@ estimated = mean(p);
 
 
 for i = 1:TS_PF
+    % Find tagged shark in range of robot
+    [i_range, x_range, y_range] = getNearbyTags(robots,range, x(i,:),y(i,:));
+    N_inRange = size(i_range, 2);
+    
     disp(i)
     numshark_old(:,2) = numshark_old(:,1); % keep track of n-2
     numshark_old(:,1) = p(:,5);
     
-    p = propagate(p, Sigma_mean, numshark_old(:,2), LINE_START, LINE_END);
-    
-    w = getParticleWeights(p, x_tagged(i,:), y_tagged(i,:), @fit_sumdist_sd, @fit_sumdist_mu, numshark_sd);
+    p = propagate(p, Sigma_mean, numshark_old(:,2), LINE_START, LINE_END);   
+    w = getParticleWeights(p, x_range, y_range, @fit_sumdist_sd, @fit_sumdist_mu, numshark_sd);
     p = resample(p,w);
     p_mean = computeParticleMean(p,w)
     
+    % Move Robot
+    robots = moveRobots(robots, x_range, y_range, ...
+        [p_mean(1) p_mean(2)], [p_mean(3) p_mean(4)]);
     
-    est_error(i) = totalSharkDistance(x(i,:), y(i,:), [p_mean(1), p_mean(2)], [p_mean(3), p_mean(4)]);
+    
+    est_error(i) = totalSharkDistance(x(i,:), y(i,:), LINE_START, LINE_END);
     act_error(i) = totalSharkDistance(x(i,:), y(i,:), LINE_START, LINE_END);
 
     % Performance Error: Error between actual and estimated line
@@ -54,17 +67,31 @@ for i = 1:TS_PF
         clf;
         hold on;
         
-        % Plot Tagged Sharks
-        for f=1:N_tagged
-           plot(x_tagged(i,f),y_tagged(i,f),'x'); 
-           plot([x_tagged(i,f) x_tagged(i,f)+cos(t_tagged(i,f))*arrowSize],[y_tagged(i,f) y_tagged(i,f)+sin(t_tagged(i,f))*arrowSize]); 
+        % Plot in range sharks
+        for f = i_range
+           plot(x(i,f),y(i,f),'x'); 
+           plot([x(i,f) x(i,f)+cos(t(i,f))*arrowSize],[y(i,f) y(i,f)+sin(t(i,f))*arrowSize]); 
         end
+        
+%         % Plot Tagged Sharks
+%         for f=1:N_tagged
+%            plot(x_tagged(i,f),y_tagged(i,f),'x'); 
+%            plot([x_tagged(i,f) x_tagged(i,f)+cos(t_tagged(i,f))*arrowSize],[y_tagged(i,f) y_tagged(i,f)+sin(t_tagged(i,f))*arrowSize]); 
+%         end
         
         
         % Plot Sharks
-        for f = N_tagged+1 : N_fish
+        for f = 1 : N_fish
            plot(x(i,f),y(i,f),'o'); 
            plot([x(i,f) x(i,f)+cos(t(i,f))*arrowSize],[y(i,f) y(i,f)+sin(t(i,f))*arrowSize]); 
+        end
+        
+        % Plot Robots
+        for r = 1 : N_robots
+            plot(robots(r,1),robots(r,2),'square',...
+                'MarkerSize',20, 'MarkerFaceColor', [.49 1 .63]);
+            circle(robots(r,1),robots(r,2), range);
+            
         end
 
         % Plot Particles
@@ -89,4 +116,13 @@ end
 % Performance Criteria for numshark estimation
 within_ns_limit = size(find( numshark_est > N_fish - 5 & numshark_est < N_fish + 5),1);
 
+end
+
+
+function h = circle(x,y,r)
+% Plot Circle given point and radius
+    th = 0:pi/50:2*pi;
+    xunit = r * cos(th) + x;
+    yunit = r * sin(th) + y;
+    h = plot(xunit, yunit);
 end
---
+++ fishSim/moveRobots.m
@@ -0,0 +1,59 @@
+function [robots] = moveRobots(robots, x_range, y_range, att_line_start, att_line_end)
+% Move robots according to fish movement model
+
+% Movement constants
+v=1.0;
+deltaT = 0.1;
+fishInteractionRadius = 1.5;
+K_con = 0.05;
+K_rep = 1e5;
+K_att = 1e3;
+K_rand = 0.1;
+sigmaRand = 0.1;
+
+N_robots = size(robots, 1);
+N_range = size(x_range,2);
+
+x_robots = robots(:,1);
+y_robots = robots(:,2);
+t_robots = robots(:,3);
+
+%loop over robot to update state
+for f=1:N_robots
+
+    % Check for neighbors (Repulsive)
+    x_rep = 0; y_rep = 0;
+    for g=1:N_range
+        dist = sqrt((x_robots(f)-x_range(g))^2 + (y_robots(f)-y_range(g))^2);
+        if dist < fishInteractionRadius
+            mag = (1/dist - 1/fishInteractionRadius)^2;
+            x_rep = x_rep+mag*(x_robots(f)-x_range(g));
+            y_rep = y_rep+mag*(y_robots(f)-y_range(g));     
+        end
+    end
+
+    % Determine attraction to habitat
+    closest_pt = project_point_to_line_segment(att_line_start, att_line_end, [x_robots(f), y_robots(f)]);
+    dist = sqrt((x_robots(f)-closest_pt(1))^2 + (y_robots(f)-closest_pt(1))^2);
+    mag = (closest_pt(1) -x_robots(f))^2 + (closest_pt(2)-y_robots(f))^2;
+    x_att = mag*(closest_pt(1)-x_robots(f));
+    y_att = mag*(closest_pt(2)-y_robots(f));
+
+    % Sum all potentials
+    x_tot = K_att*x_att + K_rep*x_rep;
+    y_tot = K_att*y_att + K_rep*y_rep;
+    desiredTheta = atan2(y_tot, x_tot);
+
+    % Set yaw control
+    maxControl = pi/180*20;
+    controlTheta = K_con*angleDiff(desiredTheta-t_robots(f)) + sigmaRand*randn(1);
+    controlTheta = min(max(controlTheta,-maxControl), maxControl);
+
+    % Update the state
+    robots(f,3) = robots(f,3) + controlTheta; % t
+    robots(f,1) = robots(f,1) + v*deltaT*cos(robots(f,3)); % x
+    robots(f,2) = robots(f,2) + v*deltaT*sin(robots(f,3)); % y
+
+
+end
+
