d2ed906796064517d36be1231f5a41122e5b3fbd
hocherie <ho.cherie@gmail.com>
2016-06-22 10:48:18 -0700

    Attempting to make transition matrix for grid

--- fishSim/transitionMatrix.m
+++ fishSim/transitionMatrix.m
@@ -7,32 +7,38 @@
 % Discretize location to node number
 
 %% For Simulation Data
-% ts_len = length(x); % Number of ts
-% ns = size(x,2);
-% dist = zeros(ts_len, ns);
-% 
-% for i = 1:ts_len
-%     for j = 1:ns
-% %         is_above = isAbove(x(i,j), y(i,j), [-25 0], [25 0]);
-% %         dist(i,j) = is_above * point_to_line(x(i,j), y(i,j), [-25 0], [25 0]);
-%         dist(i,j) = y(i,j);
-%     end
-% end
+ts_len = length(x); % Number of ts
+ns = size(x,2);
+vert_dist = zeros(ts_len, ns);
+
+for i = 1:ts_len
+    for j = 1:ns
+%         is_above = isAbove(x(i,j), y(i,j), [-25 0], [25 0]); 
+%         dist(i,j) = is_above * point_to_line(x(i,j), y(i,j), [-25 0], [25 0]);
+        vert_dist(i,j) = y(i,j);
+    end
+end
 
 %% For Actual Data
-ts_len = length(x_sharks); % Number of ts
-ns = size(x_sharks,2);
-dist = zeros(ts_len, ns);
-dist = distLine;
+% ts_len = length(x_rot); % Number of ts
+% ns = size(x_rot,2);
+% vert_dist = zeros(ts_len, ns);
+% vert_dist = distLine;
 % 
-max_dist = max(abs(dist(:)))
-max_dist = 7;
-edges = -max_dist:0.25:max_dist; % Create hist bin edges
-N = length(edges);
-Y_node = discretize(dist,edges); % Location to discretized index
+max_vert_dist = max(abs(vert_dist(:)))
+max_vert_dist = 7;
+y_edges = -max_vert_dist:3.5:max_vert_dist; % Create hist bin edges
+N_y = length(y_edges);
+Y_node = discretize(vert_dist,y_edges); % Location to discretized index
+
+x_edges = [-20:0.5:20];
+N_x = length(x_edges);
+X_node = discretize(x, x_edges);
+
+node = N_x*Y_node + X_node;
 
 %% Compute transition matrix (only for one shark)
-T = zeros(N, N);
+T = zeros(N_y*N_x, N_y*N_x);
 
 for ts = 1 : ts_len - 1
     for shark = 1:ns
@@ -44,12 +50,12 @@ end
 
 % Normalize transition matrix
 
-for from = 1:N
+for from = 1:N_x*N_y
     sum_from = sum(T(:,from));
     T(:,from) = T(:,from)/sum(T(:,from));
     
     if sum_from == 0
-        T(:,from) = zeros([1 N]);
+        T(:,from) = zeros([1 N_y*N_x]);
     end
     
 end
