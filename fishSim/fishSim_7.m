d7db2a6806ed1a13a78026c55c6f987f2e1e58a3
Cherie Ho <ho.cherie@gmail.com>
2016-06-23 17:03:54 -0700

    Added attraction to line

--- fishSim/fishSim_7.m
+++ fishSim/fishSim_7.m
@@ -5,32 +5,29 @@ function [x,y, t] = fishSim_7(N_fish, seg_length)
 maxTime = 3000;
 v=0.3;
 deltaT = 1/30;
-fishInteractionRadius = 1.5;
+fishInteractionRadius = 1;
 K_con = 0.05;
-K_rep = 5e6;
+K_rep = 1e7;
 K_att = 1e3;
 K_rand = 0.1;
+K_temp_att = 1e7;
 sigmaRand = 0.1;
-
-% LINE_START = [-25 5.533]; % Actual Line
-% LINE_END = [25 -5.3070];
 
 % Same as actual line length
 
 LINE_START = [-seg_length/2 0];
 LINE_END = [seg_length/2 0];
-% LINE_START = [-25.58 0];
-% LINE_END = [25.58 0];
 
 
 % Initialize states
 % seg_length = 50;
+height = 0;
 t=zeros(maxTime,N_fish);
 x=t;
 y=t;
 t(1,1:N_fish) = -ones(N_fish,1)*pi+rand([N_fish,1])*2*pi;
 x(1,1:N_fish) = -ones(N_fish,1)*seg_length/2+rand([N_fish,1])*seg_length;
-y(1,1:N_fish) = -ones(N_fish,1)*seg_length/2+rand([N_fish,1])*seg_length;
+y(1,1:N_fish) = -ones(N_fish,1)*height/2+rand([N_fish,1])*height;
 closeToNeighbor = zeros(maxTime,N_fish);
 
 
@@ -43,27 +40,31 @@ for time=2:maxTime;
     for f=1:N_fish
 
         % Check for neighbors
-        x_rep = 0; y_rep = 0;closeToNeighbor(time,f) = 0;
-        for g=1:N_fish
+        x_rep = 0; y_rep = 0; x_att = 0; y_att = 0;
+        for g=1:N_fish % Fish Schooling Dynamics
             dist = sqrt((x(time-1,f)-x(time,g))^2 + (y(time-1,f)-y(time-1,g))^2);
-            if dist < fishInteractionRadius && g ~= f
+            if dist < fishInteractionRadius && g ~= f % Repulsed
                 mag = (1/dist - 1/fishInteractionRadius)^2;
                 x_rep = x_rep+mag*(x(time-1,f)-x(time-1,g));
                 y_rep = y_rep+mag*(y(time-1,f)-y(time-1,g));     
-                closeToNeighbor(time,f) = closeToNeighbor(time,f)+1;
+            end
+            if dist > fishInteractionRadius && g ~= f % Attracted
+                mag = dist^2;
+                x_att = x_att+mag*(x(time-1,g)-x(time-1,f));
+                y_att = y_att+mag*(y(time-1,g)-y(time-1,f));     
             end
         end
         
         % Determine attraction to habitat
         closest_pt = project_point_to_line_segment(LINE_START, LINE_END, [x(time-1,f), y(time-1,f)]);
-        dist = sqrt((x(time-1,f)-closest_pt(1))^2 + (y(time-1,f)-closest_pt(1))^2);
+%         dist = sqrt((x(time-1,f)-closest_pt(1))^2 + (y(time-1,f)-closest_pt(1))^2);
         mag = (closest_pt(1) -x(time-1,f))^2 + (closest_pt(2)-y(time-1,f))^2;
-        x_att = mag*(closest_pt(1)-x(time-1,f));
-        y_att = mag*(closest_pt(2)-y(time-1,f));
+        x_temp_att = mag*(closest_pt(1)-x(time-1,f));
+        y_temp_att = mag*(closest_pt(2)-y(time-1,f));
    
         % Sum all potentials
-        x_tot = K_att*x_att + K_rep*x_rep;
-        y_tot = K_att*y_att + K_rep*y_rep;
+        x_tot = K_att*x_att + K_rep*x_rep + K_temp_att*x_temp_att;
+        y_tot = K_att*y_att + K_rep*y_rep + K_temp_att*y_temp_att;
         desiredTheta = atan2(y_tot, x_tot);
         
         % Set yaw control
--- fishSim/visualize.m
+++ fishSim/visualize.m
@@ -6,22 +6,22 @@ clf
 
 maxTime = size(x,1);
 N_fish = size(x,2);
-for time=1:maxTime;
+for time=1:3:maxTime;
 %     loop over fish to plot
     arrowSize = 1.5;
     fig = figure(1);
-    clf;
+%     clf;
     hold on;
     for f=1:N_fish
-       plot(x(time,f),y(time,f),'o'); 
-       plot([x(time,f) x(time,f)+cos(t(time,f))*arrowSize],[y(time,f) y(time,f)+sin(t(time,f))*arrowSize]); 
+       plot(x(time,f),y(time,f),'.'); 
+%        plot([x(time,f) x(time,f)+cos(t(time,f))*arrowSize],[y(time,f) y(time,f)+sin(t(time,f))*arrowSize]); 
     end
 
    
 
     % Plot attraction line
 %     plot([LINE_START(1), LINE_END(1)],[LINE_START(2), LINE_END(2)])
-    width = 60;
+    width = 15;
     scale = 0.5;
     axis(scale*[-width width -width width]);
 
