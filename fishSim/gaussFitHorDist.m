f96f1f13181dc8fc1a47a0657f21467100f8a818
hocherie <ho.cherie@gmail.com>
2016-09-10 18:49:33 -0700

    Merge remote-tracking branch 'origin/fix_PF_line' into fix_PF_line
    # Conflicts:
    #	fishSim/getProbFromError.m
    #	fishSim/pf_line.mat

--- fishSim/gaussFitHorDist.m
+++ fishSim/gaussFitHorDist.m
@@ -1,18 +1,19 @@
 % Gauss Fit MaxDist for attraction line PF
-seg_list = 100;
-numshark_list = [10, 30, 50, 70, 100, 150, 200];
-% numshark_list = [5, 10];
+seg_list = [10 20 30 50 70 100 200];
+numshark_list = [10 30 50 80 100 200];
 TS_PF = 1000;
 
 seg_list_len = size(seg_list,2);
 numshark_len = size(numshark_list,2);
 muhat_list_hor_dist = zeros(seg_list_len,numshark_len);
 sigmahat_list_hor_dist = zeros(seg_list_len,numshark_len);
-N_trial = 10;
+muhat_list_d90 = zeros(seg_list_len,numshark_len);
+sigmahat_list_d90 = zeros(seg_list_len,numshark_len);
+N_trial = 3;
 clf
 
 tic
-for i = 1:seg_list_len
+parfor i = 1:seg_list_len
     seg_length = seg_list(i);
     LINE_START = [-seg_length/2 0];
     LINE_END = [seg_length/2 0];
@@ -22,10 +23,11 @@ for i = 1:seg_list_len
         N_fish = numshark_list(j)
         
         edge_distance_list = zeros(N_trial, N_fish, TS_PF);
-        parfor k = 1:N_trial
+        d90_list = zeros(N_trial, N_fish, TS_PF);
+        for k = 1:N_trial
             [x,y,t] = fishSim_7(N_fish,seg_length, 1e3, 1e6, 1e9);
             for ts = 1:TS_PF
-                edge_distance_list(k,:,ts) = ...
+                [edge_distance_list(k,:,ts), d90_list(k,:,ts)] = ...
                     measureEdgeDistance(x(i,:), y(i,:), LINE_START, LINE_END);
             end
         end
@@ -33,6 +35,10 @@ for i = 1:seg_list_len
         [muhat_hor_dist, sigmahat_hor_dist] = normfit(edge_distance_list(:));
         muhat_list_hor_dist(i,j) = muhat_hor_dist
         sigmahat_list_hor_dist(i,j)= sigmahat_hor_dist;
+        
+        [muhat_ver_dist, sigmahat_ver_dist] = normfit(d90_list(:));
+        muhat_list_d90(i,j) = muhat_ver_dist
+        sigmahat_list_d90(i,j)= sigmahat_ver_dist;
     
     end
     
@@ -40,17 +46,30 @@ for i = 1:seg_list_len
 end
 
 save('gaussFitHorDist.mat','numshark_list', 'seg_list','muhat_list_hor_dist',...
-    'sigmahat_list_hor_dist')
+    'sigmahat_list_hor_dist', 'muhat_list_d90','sigmahat_list_d90')
 toc
-
+% % 
 % figure
-% subplot(2,1,1)
+% subplot(2,2,1)
 % plot(numshark_list, muhat_list_hor_dist, 'x');
-% title('Gaussian fit of Horizontal Distance') 
-% ylabel('Mean from Gaussian Fit')
-% legend(cellstr(num2str([10 20 30 50 70 100 200]')))
-% subplot(2,1,2)
+% title('Gaussian fit of Horizontal Distance for L = 100') 
+% ylabel('Mean from Gaussian Fit (m)')
+% xlabel('Number of Sharks')
+% legend(cellstr(num2str(numshark_list', 'N = %-d')))
+% subplot(2,2,3)
+% title('\sigma of Horizontal Distance for L = 100') 
 % plot(numshark_list, sigmahat_list_hor_dist, 'x')
 % xlabel('Number of Sharks')
 % ylabel('Sigma from Gaussian Fit')
-% legend(cellstr(num2str([10 20 30 50 70 100 200]')))
+% subplot(2,2,2)
+% plot(numshark_list, muhat_list_ver_dist, 'x');
+% title('Gaussian fit of Vertical Distance for L = 100') 
+% ylabel('Mean from Gaussian Fit (m)')
+% xlabel('Number of Sharks')
+% legend(cellstr(num2str(numshark_list', 'N = %-d')))
+% subplot(2,2,4)
+% title('\sigma of Vertical Distance for L = 100') 
+% plot(numshark_list, sigmahat_list_ver_dist, 'x')
+% xlabel('Number of Sharks')
+% ylabel('Sigma from Gaussian Fit')
+% 
--- fishSim/measureEdgeDistance.m
+++ fishSim/measureEdgeDistance.m
@@ -1,12 +1,17 @@
-function dist = measureEdgeDistance(x_sharks, y_sharks, line_start, line_end)
+function [dist_x, d90] = measureEdgeDistance(x_sharks, y_sharks, line_start, line_end)
 % Finds width of line given line and leftmost and rightmost fish
 
 rotAngle = atan2((line_end(2)-line_start(2)),(line_end(1)-line_start(1)));
 
 xRot     = x_sharks*cos(rotAngle) - y_sharks*sin(rotAngle);
-% yRot     = x_sharks*sin(rotAngle) + y_sharks*cos(rotAngle);
+yRot     = x_sharks*sin(rotAngle) + y_sharks*cos(rotAngle);
 
 leftmost = min(xRot(:));
 rightmost = max(xRot(:));
+topmost = max(yRot(:));
+bottommost = min(yRot(:));
 
-dist = rightmost - leftmost;
+dist_x = rightmost - leftmost;
+d90 = prctile(yRot, 95);
+
+end
